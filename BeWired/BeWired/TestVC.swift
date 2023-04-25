ef475a86f8856bbc7a527838c0699bd18ada4cd6
DOKononov <d.o.kononov@gmail.com>
2023-04-25 20:44:15 +0300

    init

---
+++ BeWired/BeWired/TestVC.swift
@@ -0,0 +1,19 @@
+//
+//  TestVC.swift
+//  BeWired
+//
+//  Created by Dmitry Kononov on 25.04.23.
+//
+
+import UIKit
+
+class TestVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
